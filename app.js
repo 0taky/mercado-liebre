fa72c2feed1a1fb6fdb1b46592833357e6ca1683
0taky <santiagogcanavide@gmail.com>
2023-05-24 22:28:07 -0300

    2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static('public'))
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 })
